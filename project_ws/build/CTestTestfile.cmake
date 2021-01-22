ffc91e89ab57502fba523fa64288af1bbf28ea80
Henry <sim1997@graduate.utm.my>
2021-01-22 13:05:22 +0800

    Robot model from scratch

---
+++ project_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/henry/project_ws/src
+# Build directory: /home/henry/project_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
